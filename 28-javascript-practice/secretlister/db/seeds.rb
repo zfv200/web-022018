f5c818e2be46487f88a41f738aa9d6d787f427da
github username <github email address>
2018-04-12 14:00:18 -0400

    seeds

--- 28-javascript-practice/secretlister/db/seeds.rb
+++ 28-javascript-practice/secretlister/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(description: "save the queen", priority: "HIGH!!")
+Task.create(description: "wash the dog", priority: "medium")
+Task.create(description: "save world", priority: "extra high")
+Task.create(description: "groceries", priority: "medium")
